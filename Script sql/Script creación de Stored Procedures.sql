6d49ce9c296eec80767c1d89ab1fe3bac2e0e580
andresbellas <andresbellas2004@gmail.com>
2026-04-03 05:07:59 -0300

    cambio en stored procedure
    ahora devuelve filas agregadas

--- Script sql/Script creación de Stored Procedures.sql
+++ Script sql/Script creación de Stored Procedures.sql
@@ -1,22 +1,44 @@
-use Proyecto_Final_Integrador 
-go
+USE Proyecto_Final_Integrador;
+GO
 
-Create or alter Procedure RegistrarUsuariosNuevos
-	@Email nvarchar(256),
-	@Password nvarchar(256),
-	@Nombre nvarchar(100),
-	@Apellido nvarchar(100),
-	@Telefono nvarchar(20)
+CREATE OR ALTER PROCEDURE RegistrarUsuariosNuevos
+    @Email NVARCHAR(256),
+    @Password NVARCHAR(256),
+    @Nombre NVARCHAR(100),
+    @Apellido NVARCHAR(100),
+    @Telefono NVARCHAR(20)
 AS
-BEGIN 
-	Insert into Usuario (Email, PasswordHash, Nombre, Apellido, Telefono)
-	values (@Email, @Password, @Nombre, @Apellido, @Telefono);
-	
-	Declare @Id as int; 
-	Set @Id = SCOPE_IDENTITY();
+BEGIN
+    SET NOCOUNT OFF;  -- para que devuelva filas afectadas
 
-	Insert into Cliente (IdUsuario, Provincia, Departamento, Localidad, Direccion) values (@Id, 'No ingresado', 'No ingresado','No ingresado', 'No ingresado');
-	Insert into Prestador (IdUsuario, Descripcion) values (@Id, 'No ingresado'); 
-	Insert into ZonasPrestador(IdPrestador, IdLocalidad) values (@Id, '');
-	insert into ZonasPrestador(IdPrestador, IdZona) values (@Id,'No ingresado')
-END; 
+    BEGIN TRY
+        BEGIN TRANSACTION;
+       
+        INSERT INTO Usuario (Email, PasswordHash, Nombre, Apellido, Telefono)
+        VALUES (@Email, @Password, @Nombre, @Apellido, @Telefono);
+
+        DECLARE @Id INT;
+        SET @Id = SCOPE_IDENTITY();
+     
+        INSERT INTO Cliente (IdUsuario, Provincia, Departamento, Localidad, Direccion)
+        VALUES (@Id, 'No ingresado', 'No ingresado', 'No ingresado', 'No ingresado');
+
+        INSERT INTO Prestador (IdUsuario, Descripcion)
+        VALUES (@Id, 'No ingresado');
+
+        INSERT INTO ZonasPrestador (IdPrestador, IdLocalidad)
+        VALUES (@Id, 'No ingresado');
+
+        INSERT INTO ZonasPrestador (IdPrestador, IdZona)
+        VALUES (@Id, 'No ingresado');
+
+        COMMIT TRANSACTION;
+      
+        SELECT 1 AS FilasAfectadas;
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        
+        SELECT 0 AS FilasAfectadas;
+    END CATCH
+END;
